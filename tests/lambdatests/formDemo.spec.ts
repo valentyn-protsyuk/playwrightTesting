e28856d8bb86f5a193086a1cc8e15988ea4eb53a
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-11-28 22:23:24 -0800

    moved form demo file

---
+++ tests/lambdatests/formDemo.spec.ts
@@ -0,0 +1,19 @@
+import {test as it} from '@playwright/test';
+
+it.describe('FORM DEMO TESTING', () => {
+    it('Fill all fields', async ({page}) => {
+        await page.goto('https://www.lambdatest.com/selenium-playground/input-form-demo');
+        await page.locator('#name').fill('testName');
+        await page.locator('#inputEmail4').pressSequentially('testEmail@yahoo.com',{delay: 300});
+
+        await page.locator('input[placeholder="Password"]').fill('testPass');
+
+        await page.locator('#company').fill('Test Co.');
+
+        await page.selectOption('select[name="country"]', {label: 'United States'});
+
+        await page.getByPlaceholder('Address 1').fill('LA');
+        
+        await page.getByRole('textbox', {name:"Address 2"}).fill('My street');
+    });
+});
