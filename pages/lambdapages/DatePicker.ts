0d298c8b64ec126d2fe445ea50632baf47048649
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-11-29 14:52:28 -0800

    date picker verify header

--- pages/lambdapages/DatePicker.ts
+++ pages/lambdapages/DatePicker.ts
@@ -1,7 +1,16 @@
-import {Page} from '@playwright/test'
+import {Page, expect} from '@playwright/test'
 export class DatePicker {
     readonly page:Page
     constructor(page:Page){
         this.page = page;
+    }
+
+    get header(){
+        return "h1";
+    }
+
+    async verifyHeader(){
+        const header = this.page.locator(this.header)
+        await expect(header).toContainText('Date Picker')
     }
 }
--- tests/lambdatests/datePicker.spec.ts
+++ tests/lambdatests/datePicker.spec.ts
@@ -4,5 +4,7 @@ import { DatePicker } from '../../pages/lambdapages/DatePicker';
 it.describe('DATE PICKER DEMO', () => {
     it('working with date picker', async ({page}) => {
         const datePicker = new DatePicker(page);
+        await page.goto('https://www.lambdatest.com/selenium-playground/bootstrap-date-picker-demo')
+        await datePicker.verifyHeader();
     });
 });
