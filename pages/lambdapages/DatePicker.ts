143b5e9648926899f93869e896d6590238ee0cac
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-11-29 15:31:22 -0800

    date picker getters

--- pages/lambdapages/DatePicker.ts
+++ pages/lambdapages/DatePicker.ts
@@ -9,6 +9,37 @@ export class DatePicker {
         return "h1";
     }
 
+    get fromInput(){
+        return "#from";
+    }
+
+    get toInput(){
+        return "#to";
+    }
+
+    get month (){
+        return '[class="ui-datepicker-month"]';
+    }
+
+    get prevYear(){
+        return '[title="Prev"]';
+    }
+    get nextYear(){
+        return '[title="Next"]';
+    }
+    get getDate(){
+        return '.ui-state-default';
+    }
+    get dateFromComponent(){
+        return '#ui-datepicker-div';
+    }
+    get dateOfTheYear(){
+        return '[class="ui-datepicker-year"]';
+    }
+    get dateOfTheMonth(){
+        return '[class="ui-datepicker-month"]';
+    }
+
     async verifyHeader(){
         const header = this.page.locator(this.header)
         await expect(header).toContainText('Date Picker')
