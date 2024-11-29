367ed89ef8cba13c0db2ea56caab3ee8e5df56b4
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-11-28 21:11:30 -0800

    form demo fill in the name

--- tests/formDemo.spec.ts
+++ tests/formDemo.spec.ts
@@ -3,6 +3,7 @@ import {test as it} from '@playwright/test';
 it.describe('FORM DEMO TESTING', () => {
     it('Fill all fields', async ({page}) => {
         await page.goto('https://www.lambdatest.com/selenium-playground/input-form-demo');
-        
+        await page.locator('#name').fill('testName');
+       
     });
 });
