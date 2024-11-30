6755b826e66ef9488110e8f99e2cac5fdb0247ba
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-11-29 21:42:00 -0800

    drag and drop V1

--- tests/lambdatests/dragDrop.spec.ts
+++ tests/lambdatests/dragDrop.spec.ts
@@ -5,6 +5,8 @@ it.describe('DRAG & DROP DEMO', () => {
     it('working with drag & drop', async ({page}) => {
         const dragDrop = new DragDrop(page);
         await page.goto('https://www.lambdatest.com/selenium-playground/drag-and-drop-demo')
-        
+        await dragDrop.dragAndDropElementV1('Draggable 1');
+        await dragDrop.dragAndDropElementV1('Draggable 2');
+        //await page.reload() //refresh page
     });
 });
