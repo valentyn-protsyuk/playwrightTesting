dba57a9b88464f57bdd3d01122ad21c2c2688743
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-11-29 18:31:59 -0800

    date picker select random date

--- pages/lambdapages/DatePicker.ts
+++ pages/lambdapages/DatePicker.ts
@@ -1,10 +1,13 @@
 import {Page, expect} from '@playwright/test'
+import _ from 'lodash'
 export class DatePicker {
+    //boilerplate constructor
     readonly page:Page
     constructor(page:Page){
         this.page = page;
     }
 
+    //getters
     get header(){
         return "h1";
     }
@@ -40,8 +43,40 @@ export class DatePicker {
         return '[class="ui-datepicker-month"]';
     }
 
+    //setters
+
+    //functions
     async verifyHeader(){
         const header = this.page.locator(this.header)
         await expect(header).toContainText('Date Picker')
+    }
+
+    randomYear = _.random(1,20);
+    randomDate = _.random(1,15);
+
+    async dateFromToday(){
+        let obj ={
+            Jan:'01',
+            Feb:'02',
+            Mar:'03',
+            Apr:'04',
+            May:'05',
+            Jun:'06',
+            Jul:'07',
+            Aug:'08',
+            Sep:'09',
+            Oct:'10',
+            Nov:'11',
+            Dec:'12'
+        }
+        await this.page.locator(this.fromInput).click()
+        for(let i = 0; i<this.randomYear; i++){
+            await this.page.locator(this.prevYear).click()
+        }
+        let year = await this.page.locator(this.dateOfTheYear).textContent()
+        let month = await this.page.locator(this.dateOfTheMonth).locator('[selected="selected"]').textContent()
+
+        await this.page.locator(this.dateFromComponent).getByRole('link',{name: this.randomDate}).click()
+        expect(await this.page.locator(this.fromInput).inputValue()).toBe(`${obj[month]}/${this.randomDate}/${year}`)
     }
 }
--- tests/lambdatests/datePicker.spec.ts
+++ tests/lambdatests/datePicker.spec.ts
@@ -1,10 +1,16 @@
 import {test as it} from '@playwright/test';
 import { DatePicker } from '../../pages/lambdapages/DatePicker';
+import { beforeEach } from 'node:test';
 
 it.describe('DATE PICKER DEMO', () => {
     it('working with date picker', async ({page}) => {
         const datePicker = new DatePicker(page);
-        await page.goto('https://www.lambdatest.com/selenium-playground/bootstrap-date-picker-demo')
+        await page.goto('https://www.lambdatest.com/selenium-playground/jquery-date-picker-demo')
         await datePicker.verifyHeader();
+    });
+    it('select date', async ({page}) => {
+        const datePicker = new DatePicker(page);
+        await page.goto('https://www.lambdatest.com/selenium-playground/jquery-date-picker-demo')
+        await datePicker.dateFromToday();
     });
 });
