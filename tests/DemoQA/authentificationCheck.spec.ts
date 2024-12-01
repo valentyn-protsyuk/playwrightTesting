038ee2abd66eebcf0d16b9a78a2d153f8ea0a2c4
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-11-30 21:48:02 -0800

    authentification test

--- tests/DemoQA/authentificationCheck.spec.ts
+++ tests/DemoQA/authentificationCheck.spec.ts
@@ -5,3 +5,7 @@ it.beforeEach(async ({page})=>{
     await page.goto('https://demoqa.com/login')
 })
 
+it('authentication check',async ({page,browserName})=>{
+    await expect(page.locator('#submit')).toHaveText('Log out')
+    await page.screenshot({path:`screenShot/${browserName}.png`})
+})
