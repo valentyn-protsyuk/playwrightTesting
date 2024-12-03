70aa274dd74cadcc85ba0e114b19388269f00630
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-12-02 22:16:35 -0800

    pop ups accept/decline

--- tests/RSAcademy/popUps.spec.ts
+++ tests/RSAcademy/popUps.spec.ts
@@ -1,8 +1,19 @@
 import {expect, test as it} from '@playwright/test'
 
 it.describe('POP UPS', () => {
-    it('test pop ups',async ({page}) => {
+    it('test accept pop up',async ({page}) => {
         await page.goto('https://rahulshettyacademy.com/AutomationPractice/')
+        await page.pause()
+        page.on('dialog', dialog => dialog.accept())
+        await page.locator('#confirmbtn').click()
+
+    });
+
+    it('test dismiss pop up',async ({page}) => {
+        await page.goto('https://rahulshettyacademy.com/AutomationPractice/')
+        await page.pause()
+        page.on('dialog', dialog => dialog.dismiss())
+        await page.locator('#confirmbtn').click()
 
     });
 });
