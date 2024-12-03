dc4cff50567799f790b893d16d1390f54c0d033d
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-12-02 21:51:10 -0800

    hide spec functionality test

--- tests/RSAcademy/hide.spec.ts
+++ tests/RSAcademy/hide.spec.ts
@@ -1,8 +1,10 @@
-import {test as it} from '@playwright/test'
+import {expect, test as it} from '@playwright/test'
 
 it.describe('HIDE FUNCTION', () => {
     it('test hide function',async ({page}) => {
         await page.goto('https://rahulshettyacademy.com/AutomationPractice/')
-        
+        await expect(page.locator('#displayed-text')).toBeVisible()
+        await page.locator('#hide-textbox').click()
+        await expect(page.locator('#displayed-text')).toBeHidden()
     });
 });
