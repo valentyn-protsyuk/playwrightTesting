eb6a55a372923be44d8b2431a6b76a08f6dde4ff
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-12-02 20:46:53 -0800

    drag and drop link fix

--- tests/lambdatests/dragDrop.spec.ts
+++ tests/lambdatests/dragDrop.spec.ts
@@ -4,7 +4,7 @@ import { DragDrop } from '../../pages/lambdapages/DragDrop';
 it.describe('DRAG & DROP DEMO', () => {
     it('working with drag & drop', async ({page}) => {
         const dragDrop = new DragDrop(page);
-        await page.goto(`${process.env.LAMBDA_PLAYGROUND_URL}/drag-and-drop-demo`)
+        await page.goto(`https://www.lambdatest.com/selenium-playground/drag-and-drop-demo`)
         await dragDrop.dragAndDropElementV1('Draggable 1');
         await dragDrop.dragAndDropElementV2('Draggable 2');
         //await page.reload() //refresh page
