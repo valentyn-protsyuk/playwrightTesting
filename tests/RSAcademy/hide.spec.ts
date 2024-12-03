409be4b0fca82db63c9e845014fe98cdab7b006a
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-12-02 21:46:23 -0800

    hide spec boilerplate

---
+++ tests/RSAcademy/hide.spec.ts
@@ -0,0 +1,8 @@
+import {test as it} from '@playwright/test'
+
+it.describe('HIDE FUNCTION', () => {
+    it('test hide function',async ({page}) => {
+        await page.goto('https://rahulshettyacademy.com/AutomationPractice/')
+        
+    });
+});
