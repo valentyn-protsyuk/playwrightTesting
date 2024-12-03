7cdb9b7c25d15cdaa4a53f0ac3ec47d583351a85
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-12-02 19:40:57 -0800

    assertions spec title check

--- tests/lambdatests/assertion.spec.ts
+++ tests/lambdatests/assertion.spec.ts
@@ -1,8 +1,9 @@
-import {test as it} from '@playwright/test'
+import {test as it, expect} from '@playwright/test'
 
 it.describe('ASSERTION', () => {
     it('Assertions', async ({page}) => {
         await page.goto('https://www.lambdatest.com/')
-        
+        const title = await page.title()
+        expect(title).toContain('LambdaTest')
     });
 });
