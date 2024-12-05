d768220c5846a68a09267196f9178b53194f23ab
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-12-04 18:10:17 -0800

    check for blocked imgs errors

---
+++ tests/network/interceptCheck.spec.ts
@@ -0,0 +1,17 @@
+import {test as it} from '@playwright/test'
+
+it('Check for errors caused by Image intercept', async ({page})=>{
+    await page.route('**\/*.{png,jpg,jpeg,svg}',(body)=>{ //monitor file extension
+        if(body.request().resourceType() === 'image'){    //check for image type
+            body.abort()
+        }else{
+            body.continue()
+        }
+    })
+
+    page.on('pageerror', (err) =>{
+        console.log(`error: ${err}`);
+    })
+    
+    await page.goto('https://demoqa.com/elements')
+})
