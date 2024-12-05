c53af8bfefabe7f929c7cef733294173650fbeea
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-12-04 18:52:54 -0800

    adBlock intercept urls from array

--- tests/network/adBlock.spec.ts
+++ tests/network/adBlock.spec.ts
@@ -14,5 +14,13 @@ it('Errors caused by Image intercept', async ({page})=>{
         "https://www.gstatic.com",
         "https://cdn.ad.plus"
     ];
+    await page.route('**\/*',(route)=>{ //monitor file extension
+        const url = route.request().url();
+        if(adBlock.some(block=>url.startsWith(block))) route.abort();
+        else route.continue();
+    })
 
+    await page.pause()
+    await page.goto('https://demoqa.com/elements')
+    await page.pause()
 })
