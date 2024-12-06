f22a7160d1640cd5b915020925e4b35f05e4b7ea
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-12-05 18:03:00 -0800

    removed demoqa authentification

--- tests/DemoQA/authentificationCheck.spec.ts
+++ tests/DemoQA/authentificationCheck.spec.ts
@@ -1,11 +1,11 @@
-import {test as it,expect} from '@playwright/test'
-import test from 'node:test'
+// import {test as it,expect} from '@playwright/test'
+// import test from 'node:test'
 
-it.beforeEach(async ({page})=>{
-    await page.goto('https://demoqa.com/login')
-})
+// it.beforeEach(async ({page})=>{
+//     await page.goto('https://demoqa.com/login')
+// })
 
-it('authentication check',async ({page,browserName})=>{
-    await expect(page.locator('#submit')).toHaveText('Log out')
-    await page.screenshot({path:`screenShot/${browserName}.png`})
-})
+// it('authentication check',async ({page,browserName})=>{
+//     await expect(page.locator('#submit')).toHaveText('Log out')
+//     await page.screenshot({path:`screenShot/${browserName}.png`})
+// })
